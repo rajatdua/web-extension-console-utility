b3d987a72d5d4172109b8851625fe4fd18cf02f1
Rajat Dua <rajatdua95@gmail.com>
2020-12-27 01:18:50 +0530

    feat: service-worker

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,13 @@
 import { printLine } from './modules/print';
 
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
 printLine("Using the 'printLine' function from the Print Module");
+(function () {
+  var log = console.log;
+  console.log = function () {
+    log.call(this, 'My Console!!!', ...arguments);
+  };
+}());
+navigator.serviceWorker.register('../sw/serviceWorker.js').then(x=>console.log('done', x));
+
+printLine("End!");
---
+++ src/sw/serviceWorker.js
@@ -0,0 +1,1 @@
+self.addEventListener('fetch', e=> console.log(e));
