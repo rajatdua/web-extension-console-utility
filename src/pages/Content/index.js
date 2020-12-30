770eee7cb84887b5078025ef4cb714b1383b0a9b
Adrish Aditya <adrishaditya@gmail.com>
2020-12-30 22:13:58 +0530

    feat: inject react component

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,18 @@
-import { printLine } from './modules/print';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+function Injected(props){
+	render(
+		<div>UwU</div>
+	)
+} 
 
-printLine("Using the 'printLine' function from the Print Module");
+window.onload = ()=>{
+	let container = document.createElement('div');
+	container.setAttribute("id", "extInject");
+	container.style.position = "absolute";
+	window.document.body.appendChild(container)
+
+	console.log(window.document.querySelector('#extInject'))
+	ReactDOM.render(<div>UwU</div>, window.document.querySelector('#extInject'));
+}
